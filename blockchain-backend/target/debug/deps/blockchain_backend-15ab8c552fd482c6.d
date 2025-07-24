5683a53d5e19da40ceb26f9cef576d623cd8afe8
Leroy Dickens <36738999@mylife.unisa.ac.za>
2025-07-24 05:46:45 +0200

    Setup the EVM smart contracts directory.

---
+++ blockchain-backend/target/debug/deps/blockchain_backend-15ab8c552fd482c6.d
@@ -0,0 +1,5 @@
+/home/leroy3673/blockchain-projects/new-project/CrossFin-AI/blockchain-backend/target/debug/deps/libblockchain_backend-15ab8c552fd482c6.rmeta: src/main.rs
+
+/home/leroy3673/blockchain-projects/new-project/CrossFin-AI/blockchain-backend/target/debug/deps/blockchain_backend-15ab8c552fd482c6.d: src/main.rs
+
+src/main.rs:
---
+++ blockchain-backend/target/debug/deps/blockchain_backend-43b81d4aef804abd.d
@@ -0,0 +1,5 @@
+/home/leroy3673/blockchain-projects/new-project/CrossFin-AI/blockchain-backend/target/debug/deps/libblockchain_backend-43b81d4aef804abd.rmeta: src/main.rs
+
+/home/leroy3673/blockchain-projects/new-project/CrossFin-AI/blockchain-backend/target/debug/deps/blockchain_backend-43b81d4aef804abd.d: src/main.rs
+
+src/main.rs:
